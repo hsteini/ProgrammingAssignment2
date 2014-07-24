c6c684da143360ccddd7bdf92ff4afb98edb4b8a
hsteini <helmut@susaba.com>
2014-07-24 14:46:54 +0200

    the second change
    copied the example from the program assigment

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,31 @@
 ## my function will do the following
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- x$getmean()
+        if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
 
 
-## Write a short comment describing this function
+## This function should return a matrix that is the inverse of 'x'
 
-cacheSolve <- function(x, ...) {
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+    setmean = setmean,
+    getmean = getmean)
+}
         ## Return a matrix that is the inverse of 'x'
-}
